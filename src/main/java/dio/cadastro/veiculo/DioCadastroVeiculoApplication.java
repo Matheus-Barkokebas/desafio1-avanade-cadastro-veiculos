6cf370cf24b8db78b0e1797fa95b2ab085c2b337
Matheus <matheusbarkcaribe@gmail.com>
2025-03-08 21:09:56 -0300

    initial commit

---
+++ src/main/java/dio/cadastro/veiculo/DioCadastroVeiculoApplication.java
@@ -0,0 +1,13 @@
+package dio.cadastro.veiculo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioCadastroVeiculoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DioCadastroVeiculoApplication.class, args);
+	}
+
+}
